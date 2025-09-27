2c37349fa202b3797fcbd908c7c6b3515bf99493
Pramod <pkadus1997@gmail.com>
2025-09-28 02:17:20 +0530

    Initial commit

---
+++ src/ex_01_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Comments {
+    static void main(){
+        System.out.println("This program is for comments..");
+        //My name is Pramod
+        /* Name: Pramod
+        *  Course: ATB14x
+        *  Learning: Java
+        */
+    }
+}
