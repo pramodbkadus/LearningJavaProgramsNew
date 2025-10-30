e69be62f0bedd43fe7c59f73d590135a03634119
pramod <pkadus1997@gmail.com>
2025-10-30 22:49:47 +0530

    12th_Oct_2025

--- src/ex_16_Arrays/Lab162_2D_Array_Iterate_For_Loop.java
+++ src/ex_16_Arrays/Lab162_2D_Array_Iterate_For_Loop.java
@@ -3,7 +3,11 @@ package ex_16_Arrays;
 public class Lab162_2D_Array_Iterate_For_Loop {
     public static void main(String[] args) {
 
-        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
         
         // 3X3
         // R -> 3
